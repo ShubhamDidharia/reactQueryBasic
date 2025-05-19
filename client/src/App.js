cafbf4fb4729055bf3f84e89cc0b242c6c483dd6
ShubhamDidharia <shubhamdidharia627@gmail.com>
2025-05-19 13:02:51 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import { useQuery}  from '@tanstack/react-query';
+import Form from './components/Form';
+
+
+function App() {
+  const {data} = useQuery({
+  queryKey: ['todo'],
+  queryFn: async () => await (await fetch('http://localhost:8000/todo')).json()
+});
+
+  console.log(data);
+  return (
+    <div className="App">
+     <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+        <input type='text'/>
+        <button>Create</button>
+    </div>
+  )
+}
+
+export default Form
